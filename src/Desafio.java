556e662af79c9923f225a733b2772afe8128c278
YanGraim <yangraim@gmail.com>
2024-09-18 09:52:09 -0300

    Desafio pronto

--- src/Desafio.java
+++ src/Desafio.java
@@ -26,8 +26,28 @@ public class Desafio {
         while (opcao != 4) {
             System.out.println(menu);
             opcao = leitura.nextInt();
-            if (opcao != 4) {
-                System.out.println("Favor digitar um número válido");
+            if (opcao == 1) {
+                System.out.println("Seu saldo atual é: " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual valor você deseja transferir?");
+                double valor = leitura.nextDouble();
+                if(valor > saldo) {
+                    System.out.println("Não há saldo para realizar transferencia");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Valor transferido com sucesso!\nSeu saldo atual é de: " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Qual valor você deseja depositar?");
+                double valor = leitura.nextDouble();
+                if (valor <= 0) {
+                    System.out.println("Você não pode depositar 0 ou valores negativos");
+                } else {
+                    saldo += valor;
+                    System.out.println("Valor depositado com sucesso!\nSeu saldo atual é de: " + saldo);
+                }
+            } else if (opcao != 4) {
+                System.out.println("Opção invalida!");
             }
         }
 
